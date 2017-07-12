b1d245bd8f933b5c166eb42feb7f868e594964a9
Jeffrey A McCommas <jeffmccommas@gmail.com>
2017-07-12 15:03:31 -0400

    added front end code to logout

--- models/user.js
+++ models/user.js
@@ -1,16 +1,15 @@
-/**
- * Created by jmccommas on 7/3/17.
- */
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var mongooseUniqueValidator = require('mongoose-unique-validator');
 
- 
-var monsoose = require('mongoose');
-var Schema= mongoose.Schema;
-var mongooseUniqueValidator= require('mongoose-unique-validator');
 var schema = new Schema({
-	firstName:{type: String, required:true},
-	password:{type: String, required:true},
-	email:{type: String, required:true, unique:true},
-	messages:[{type: Schema.Types.ObjectId,ref:'Message'}]
+	firstName: {type: String, required: true},
+	lastName: {type: String, required: true},
+	password: {type: String, required: true},
+	email: {type: String, required: true, unique: true},
+	messages: [{type: Schema.Types.ObjectId, ref: 'Message'}]
 });
-module.exports=mongoose.model('User',schema);
+
 schema.plugin(mongooseUniqueValidator);
+
+module.exports = mongoose.model('User', schema);
