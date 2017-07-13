b5c5a92d87c8e66d97118be04d69eca0967c3e49
Jeffrey A McCommas <jeffmccommas@gmail.com>
2017-07-13 10:23:28 -0400

    added optimization

--- app/messages/messages.component.ts
+++ app/messages/messages.component.ts
@@ -1,9 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-messages',
     template: `
-<div class="container">
         <div class="row">
             <app-message-input></app-message-input>
         </div>
@@ -11,13 +10,8 @@ import { Component, OnInit } from '@angular/core';
         <div class="row">
             <app-message-list></app-message-list>
         </div>
-        </div>
     `
 })
 export class MessagesComponent {
-    constructor() { }
-
-    ngOnInit() { }
-
 
 }
